0570e1220e87259c0a39c15b86cba1621bd84522
Abhishek Suman <abhisheksumancode@gmail.com>
2023-04-17 19:56:00 +0530

    m1

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import Navbar from './Navbar'
-import Image from 'next/image'
 import { OrbitControls, PerspectiveCamera, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import Model from "./Model"
