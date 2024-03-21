0b33e7c29f1f914e9c073844ccebe7ba81e1ef1f
Abhishek suman <abhisheksumancode@gmail.com>
2024-03-21 16:04:20 +0530

    link updated

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -1,15 +1,15 @@
-import githubLogo from './assets/github.png';
-import linkedinLogo from './assets/ln.png';
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from "next/link"
+import githubLogo from "./assets/github.png";
+import linkedinLogo from "./assets/ln.png";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 const Hero = () => {
-  const [heading, setHeading] = useState('Engineer');
+  const [heading, setHeading] = useState("Engineer");
 
   useEffect(() => {
-    const headings = [ 'Tech Enthusiast', 'Web Developer', 'Aspiring Engineer'];
+    const headings = ["Tech Enthusiast", "Web Developer", "Aspiring Engineer"];
     let i = 0;
-    
+
     const changeHeading = () => {
       setHeading(headings[i]);
       i = i < headings.length - 1 ? i + 1 : 0;
@@ -26,57 +26,54 @@ const Hero = () => {
           <h1>Abhishek Suman</h1>
           <h4>{heading}</h4>
           <p>
-             Crafting user experiences  <br></br>that bridge creativity with functionality.
-            </p>
+            Crafting user experiences <br></br>that bridge creativity with
+            functionality.
+          </p>
         </div>
         <div className="menu">
           <a href="#about">About</a>
           <a href="#experience">Experience</a>
           <a href="#projects">Projects</a>
-      
         </div>
         <div className="social">
           <a
-            href="https://github.com/Abhishek-Sumn"
+            href="https://www.linkedin.com/in/abhishek-suman-0021411b7/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               src={linkedinLogo}
-                width={55}
-                height={33}
-                alt="Linkedin Link"
-             />
+              width={55}
+              height={33}
+              alt="Linkedin Link"
+            />
           </a>
           <a
-            href="https://www.linkedin.com/in/abhishek-suman-0021411b7/"
+            href="https://github.com/Abhishek-Sumn"
             target="_blank"
             rel="noopener noreferrer"
           >
             {/* <img className="l" src={linkedinLogo} alt="Linkedin Link" /> */}
-            <Image
-              src={githubLogo}
-                width={55}
-                height={33}
-                alt="Github Link"
-             />
+            <Image src={githubLogo} width={55} height={33} alt="Github Link" />
           </a>
           <a
-            href="https://www.linkedin.com/in/abhishek-suman-0021411b7/"
+            href="https://github.com/Abhishek-Sumn"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            
-          </a>
-         
+          ></a>
         </div>
 
         <div className="resume-link">
-          <a target='_blank' href="https://drive.google.com/file/d/1jQmzs-GRSMmyikgsswN6JXydCwowNdbm/view?usp=sharing">My Resume</a>
-          </div>
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1jQmzs-GRSMmyikgsswN6JXydCwowNdbm/view?usp=sharing"
+          >
+            My Resume
+          </a>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Hero;
